d6d5f3f9ee5279d8f518a62197b683a1279ebb2d
Alfonso Alba <alfonso@alfonsoalba.com>
2021-08-25 17:11:05 +0200

    Remove unnecesary sudo statements from provision script.

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/env bash
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
-wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
-sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > \
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | apt-key add -
+sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > \
   /etc/apt/sources.list.d/jenkins.list'
-sudo apt-get update
+apt-get update
+apt-get upgrade -y
 # Añade openjdk-8 para poder ejecutar jenkins y openjdk-16 para compilar la app en Java
-sudo apt-get install -y openjdk-8-jdk openjdk-16-jdk
+apt-get install -y openjdk-8-jdk openjdk-16-jdk
 # Selecciona Java 8 por defecto, que es la versión que utiliza Jenkins
 update-alternatives --set java /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java
 
 # Instala Jenkins
-sudo apt-get install -y jenkins
+apt-get install -y jenkins
 
 # Clone repositoriess
 git clone https://github.com/alfonsoalba-cursos/armstrong-number-c
